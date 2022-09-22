293bac0c9d6bc846ba10a710a72b131da97f9c45
William Martin <martinwillnorman@protonmail.com>
2022-09-22 08:30:07 -0400

    add Main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,35 @@
+import "./Main.css";
+
+import photoCouch from "../../assets/photo-couch.jpg";
+import photoCouchLarge from "../../assets/photo-couch_2x.jpg";
+
+import { Button } from "../Button";
+
+export function Main() {
+  return (
+    <main
+      style={{
+        backgroundImage: `image-set(url(${photoCouch}) 1x, url(${photoCouchLarge}) 2x)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="main-horizontal-split">
+        <div className="gutter">
+          <h3 className="main-h3">New Games & Accessories</h3>
+          <h1 className="main-h1">
+            Monthly Packages. <br />
+            Excitement delivered daily.
+          </h1>
+          <p className="main-p">
+            What’s the best way to shop for the latest video games and
+            peripherals? How about never shopping at all? You’ll get new stuff
+            on your doorstep — every month.
+          </p>
+
+          <Button />
+        </div>
+      </div>
+    </main>
+  );
+}
